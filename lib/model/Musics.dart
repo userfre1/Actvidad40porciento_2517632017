f4807e93635ce8e9f1797b7f6698dffef3eb3b28
userfre1 <cruzfredy619@gmail.com>
2021-11-21 20:05:43 -0600

    uploading

---
+++ lib/model/Musics.dart
@@ -0,0 +1,11 @@
+//Luis Antonio Fredy Romero Cruz
+//Carnet: 25-1763-2017
+
+class Musics {
+  String title;
+  String singer;
+  String image;
+  String url;
+
+  Musics({this.title, this.singer, this.image, this.url});
+}
